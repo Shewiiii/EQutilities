33b7005cc09f6d6a2c07674ce9db63bced662005
Shewi <anh.kiem.pho1@gmail.com>
2024-03-16 14:03:50 +0100

    More presets

--- generateAutoEQ.py
+++ generateAutoEQ.py
@@ -43,74 +43,57 @@ def adddir(dir):
     except:
         print('Dossier preset déjà existant')
 
-def batchAutoEQ(path,filenames,targetname,mode=1):
+def batchAutoEQ(path,filenames,targetname):
     '''AutoEQ tous les IEMs désignés.
     mode=1: EQ tous les IEMs à une target donnée
     mode=2: EQ un IEM à toutes les targets données
     '''
     erreurs = []
-    if mode == 1:
-        driver.find_element(By.CLASS_NAME,'upload-target').click()
-        file_input = driver.find_element(By.ID, 'file-fr')
-        file_input.send_keys(path+targetname+'.txt')
-        driver.find_element(By.CLASS_NAME,'upload-fr').click()
-        for file in filenames:
+    filenames = [filenames[0]] + filenames
+    dir = f'./presets/{targetname}'
+    adddir(f'./presets/{targetname}')
+    for type in ['Parametric','IIR','Poweramp','Wavelet']:
+        adddir(f'{dir}/{type}')
+    driver.find_element(By.CLASS_NAME,'upload-fr').click()
+    file_input = driver.find_element(By.ID, 'file-fr')
+    file_input.send_keys(path+targetname+'.txt')
+    driver.find_element(By.CLASS_NAME,'upload-target').click()
+    for file in filenames:
+        if targetname != file.replace('.txt',''):
             try:
                 file_input.send_keys(path+file)
                 driver.find_element(By.CLASS_NAME,'autoeq').click()
-                driver.find_element(By.CLASS_NAME,'export-filters').click()
-                for _ in range(2):
-                    execute(driver,'document.getElementsByClassName("remove")[2].click()')
-            except:
-                erreurs.append(file)
-
-
-    elif mode == 2:
-        filenames = [filenames[0]] + filenames
-        dir = f'./presets/{targetname}'
-        adddir(f'./presets/{targetname}')
-        for type in ['Parametric','IIR','Poweramp','Wavelet']:
-            adddir(f'{dir}/{type}')
-        driver.find_element(By.CLASS_NAME,'upload-fr').click()
-        file_input = driver.find_element(By.ID, 'file-fr')
-        file_input.send_keys(path+targetname+'.txt')
-        driver.find_element(By.CLASS_NAME,'upload-target').click()
-        for file in filenames:
-            if targetname != file.replace('.txt',''):
+                time.sleep(2.5)
                 try:
-                    file_input.send_keys(path+file)
-                    driver.find_element(By.CLASS_NAME,'autoeq').click()
-                    time.sleep(2.5)
-                    try:
-                        driver.find_element(By.CLASS_NAME,'export-filters').click()
-                    except:
-                        time.sleep(2)
-                        driver.find_element(By.CLASS_NAME,'export-filters').click()
-                    renamepath = "./rename_input"
-
-                    filename = next(walk(renamepath), (None, None, []))[2][0]
-                    newname = f"EQ to {file}"
-                    newpath = f'{renamepath}/{newname}'
-                    rename(f'{renamepath}/{filename}',newpath)
-                    final = f'{dir}/Parametric/{newname}'
-                    shutil.move(newpath,final)
-
-                    driver.find_element(By.CLASS_NAME,'export-graphic-filters').click()
-                    rename(f'{renamepath}/{filename.replace('Filters','Graphic Filters')}',newpath)
-                    shutil.move(newpath,f'{dir}/Wavelet/{newname}')
-
-                    paraToJSON(newname.replace('.txt',''),f'{dir}/Parametric',f'{dir}/Poweramp')
-                    paraToIIR(newname.replace('.txt',''),f'{dir}/Parametric',f'{dir}/IIR')
-                    execute(driver,'document.getElementsByClassName("remove")[1].click()')
+                    driver.find_element(By.CLASS_NAME,'export-filters').click()
                 except:
-                    print(traceback.format_exc())
-                    input('')
-                    erreurs.append(file)
+                    time.sleep(2)
+                    driver.find_element(By.CLASS_NAME,'export-filters').click()
+                renamepath = "./rename_input"
+
+                filename = next(walk(renamepath), (None, None, []))[2][0]
+                newname = f"EQ to {file}"
+                newpath = f'{renamepath}/{newname}'
+                rename(f'{renamepath}/{filename}',newpath)
+                final = f'{dir}/Parametric/{newname}'
+                shutil.move(newpath,final)
+
+                driver.find_element(By.CLASS_NAME,'export-graphic-filters').click()
+                rename(f'{renamepath}/{filename.replace('Filters','Graphic Filters')}',newpath)
+                shutil.move(newpath,f'{dir}/Wavelet/{newname}')
+
+                paraToJSON(newname.replace('.txt',''),f'{dir}/Parametric',f'{dir}/Poweramp')
+                paraToIIR(newname.replace('.txt',''),f'{dir}/Parametric',f'{dir}/IIR')
+                execute(driver,'document.getElementsByClassName("remove")[1].click()')
+            except:
+                print(traceback.format_exc())
+                input('')
+                erreurs.append(file)
     return erreurs
 
 erreurs = {}
 for iem in filenames[0:]:
-    erreurs[iem] = batchAutoEQ(path,filenames,iem.replace('.txt',''),mode=2)
+    erreurs[iem] = batchAutoEQ(path,filenames,iem.replace('.txt',''))
     for _ in range(2):
         execute(driver,'document.getElementsByClassName("remove")[2].click()')
 
